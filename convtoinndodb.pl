921a3bb6549914430a89a5c6ed132a12bb2a6e68
Kristian Martensen <kod@miracleas.dk>
2013-12-20 10:46:24 +0100

    cleaner formatting of innodb convert script

--- convtoinndodb.pl
+++ convtoinndodb.pl
@@ -7,17 +7,25 @@ my $dsn      = 'DBI:mysql:database=information_schema;host=localhost';
 my $dbuser   = 'root';
 my $password = '';
 my $dbh      = DBI->connect($dsn, $dbuser, $password, { RaiseError => 1, AutoCommit => 0 });
+my $sth      = $dbh->prepare("select table_schema , table_name
+                              from   information_schema.tables 
+                              where  engine like 'myisam' 
+                                and  table_schema not in ( 'mysql', 'information_schema' ) "
+                            );
 
-my $sth      = $dbh->prepare("select table_schema, table_name, engine from information_schema.tables where engine like 'myisam' and table_schema not in ( 'mysql', 'information_schema' ) ");
 $sth->execute();
+
 while (my $ref = $sth->fetchrow_hashref()) {
    print "Found a table: schema:  = $ref->{'table_schema'}, name = $ref->{'table_name'} \n";
    print "converting to innodb \n";
+
    $dbh->do("alter table $ref->{'table_schema'}.$ref->{'table_name'} engine=InnoDB");
+
    print "done with this one \n";  
 }
+
 $sth->finish();
 
-print "done with all";
+print "done with all \n";
 
 $dbh->disconnect();
